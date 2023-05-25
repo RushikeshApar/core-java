138e5a428b27c369a3b63ab94821fb5915e8f7b0
Rushikesh Apar <rushikeshapar.apar@gmail.com>
2023-05-25 14:38:19 +0530

    String concatenation

--- Desktop/JavaScript/02_Strings.js
+++ Desktop/JavaScript/02_Strings.js
@@ -30,27 +30,35 @@ console.log(firstName.toLowerCase());
 console.log(lastName.toLowerCase());
 
 // slice
-newString = firstName.slice(0, 4); // slice (start_index , last_index) 
-console.log("slice --> 0 to 5 =" ,newString);
+newString = firstName.slice(0, 4); // slice (start_index , last_index)
+console.log("slice --> 0 to 5 =", newString);
 
-
-// typeof 
+// typeof
 
 console.log(typeof firstName);
 
 let rollNo = 200;
-console.log(typeof rollNo); 
+console.log(typeof rollNo);
 
-// type casting     
+// type casting
 
-console.log("type casting"); 
+console.log("type casting");
 
 // convert string to number
-let  str1 = "rushi";   // let  str1 = + "rushi";
+let str1 = "rushi"; // let  str1 = + "rushi";
 str1 = Number(str1);
-console.log( "typeof str1 is ",typeof str1);
+console.log("typeof str1 is ", typeof str1);
 
-// convert number to string 
-let num1 = 25;      //num1 = num1 + ""
+// convert number to string
+let num1 = 25; //num1 = num1 + ""
 num1 = String(num1);
-console.log("typeof str2 is ",typeof num1);
+console.log("typeof str2 is ", typeof num1);
+
+// String concatenation
+
+let String1 = "Rushikesh";
+
+let String2 = "Apar";
+
+let fullName = String1 + " " + String2; 
+console.log(fullName);
